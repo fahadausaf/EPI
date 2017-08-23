1386d094175b9146f44ee1d05f15a3972e95ddeb
fahadausaf <fahad.ausaf@icloud.com>
2017-08-23 16:22:42 +0100

    different versions of Parity Check

--- CH01.cpp
+++ CH01.cpp
@@ -30,11 +30,33 @@ short Parity2(unsigned long x){
   return result;
 }
 
+/*
+short Parity3(unsigned long x){
+  const int kMaskSize = 16;
+  const int kBitMask = 0xFFFF;
+  return precomputed_parity[x >> (3 * kMaskSize)] ^
+         precomputed_parity[(x >> (2 * kMaskSize)) & kBitMask] ^
+         precomputed_parity[(x >> kMaskSize) & kBitMask] ^
+         precomputed_parity[x & kBitMask];
+}
+*/
+
+short Parity4(unsigned long x) {
+  x ^= x >> 32;
+  x ^= x >> 16;
+  x ^= x >> 8;
+  x ^= x >> 4;
+  x ^= x >> 2;
+  x ^= x >> 1;
+  return x & 0x1;
+}
+
 int main(){
   cout << "Chapter 1: Primitive Types" << endl;
 
   cout << "CountBits(5): " << CountBits(5) << endl;
   cout << "Parity(7): " << Parity(7) << endl;
   cout << "Parity2(7): " << Parity2(7) << endl;
+  cout << "Parity4(7): " << Parity4(7) << endl;
   return 0;
 }
