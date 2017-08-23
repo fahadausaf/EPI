430e6792e018c4949717054cbf8966c701fcc239
fahadausaf <fahad.ausaf@icloud.com>
2017-08-23 11:10:01 +0100

    Count bytes

--- CH01.cpp
+++ CH01.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-  cout << "Chapter 1: Primitive Types";
+short CountBits(unsigned int x){
+  short num_bits = 0;
+  while (x){
+    num_bits += x & 1;
+    x >>= 1;
+    //cout << "x: " << x << ", num_bits: " << num_bits << endl;
+  }
+  return num_bits;
+}
 
+int main(){
+  cout << "Chapter 1: Primitive Types" << endl;
+
+  //short s = CountBits(5);
+  cout << "CountBits: " << CountBits(5) << endl;
   return 0;
 }
