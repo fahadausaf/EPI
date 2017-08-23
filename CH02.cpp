94f1cde6b90f8bb65ac07ce4bf188de9583c858c
fahadausaf <fahad.ausaf@icloud.com>
2017-08-23 18:47:04 +0100

    vector issues

---
+++ CH02.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void EvenOdd(vector <int>* A_ptr) {
+  vector <int>& A = *A_ptr;
+  int next_even = 0, next_odd = A.size() - 1;
+  while (next_even < next_odd) {
+    if (A[next_even] % 2 == 0) {
+      ++next_even;
+    } else {
+      swap(A[next_even], A[next_odd --]);
+    }
+  }
+}
+
+int main(){
+  cout << "Chapter 2: Arrays";
+
+  vector<int> A (1,4,7,9,2,5,8,3);
+
+  return 0;
+}
