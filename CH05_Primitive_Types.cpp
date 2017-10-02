eb126d0b50d831f6bbad1e3e0ec6f00da5e5732b
Fahad Ausaf <fahad.ausaf@icloud.com>
2017-10-02 18:15:53 +0100

    1.1: Parity

--- CH05_Primitive_Types.cpp
+++ CH05_Primitive_Types.cpp
@@ -11,11 +11,20 @@ short CountBits(unsigned int x){
   return num_bits;
 }
 
+// 1.1: Computing the Parity of a Word
+short Parity(unsigned long x){
+  short result = 0;while(x){
+    result ^= (x & 1);
+    x >>= 1;
+  }
+  return result;
+}
+
 int main(){
   cout << "Chapter 1: Primitive Types" << endl;
 
   cout << "CountBits(5): " << CountBits(5) << endl;
-  //cout << "Parity(7): " << Parity(7) << endl;
+  cout << "Parity(6): " << Parity(6) << endl;
   //cout << "Parity2(7): " << Parity2(7) << endl;
   //cout << "Parity4(7): " << Parity4(7) << endl;
   return 0;
