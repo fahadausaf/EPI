06a8784765632a5f96911d82f1ae324b62a864c1
Fahad Ausaf <fahad.ausaf@icloud.com>
2018-01-25 13:59:43 +0000

    GetChar

--- GetChar.cpp
+++ GetChar.cpp
@@ -3,13 +3,28 @@
 #include <iostream>
 using namespace std;
 
+int gettok(){
+  static int LastChar = ' ';
+  cout << "Type a program" << endl;
+
+  // Skip any whitespace.
+  while (isspace(LastChar))
+    LastChar = getchar();
+
+  return 1;
+}
+
 int main ()
 {
+  /*
   int c;
   cout << "Enter text. Include a dot ('.') in a sentence to exit:" << endl;
   do {
     c=getchar();
     //putchar (c);
   } while (c != '.');
+  */
+
+  gettok();
   return 0;
 }
