3de48a6eb08c906eab6e7611ef38b0bf61b7340f
Fahad Ausaf <fahad.ausaf@icloud.com>
2018-01-25 14:41:30 +0000

    Is Palindrome

--- CH07_Strings/Is_Palindromic.cpp
+++ CH07_Strings/Is_Palindromic.cpp
@@ -2,10 +2,9 @@
 using namespace std;
 
 bool IsPalindromic(string s){
-  for(int i=0, j=s.size()-1; i<j; ++i, --j){
-    if(s[i] != s[j]) {
+  for(int i=0, j=s.size()-1; i<j; i++, j--){
+    if(s[i]!=s[j])
       return false;
-    }
   }
   return true;
 }
